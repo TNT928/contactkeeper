2e3ae2497c3e0ddfcf83af394c0f6c0a9c413c25
Michael Vanderloon <jenkins92886@gmail.com>
2020-07-30 00:59:36 -0400

    worked on update contact

--- Routes/contacts.js
+++ Routes/contacts.js
@@ -1,34 +1,107 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require('express');
+const router = express.Router();
+const {body, validationResult} = require('express-validator');
+const auth = require('../middleware/auth');
+const Contact = require('../models/Contact');
+const User = require('../models/User');
 
 // @route       GET api/contacts
 // @desc       Get all users contacts
 // @access     Private
-router.get('/',(req,res) => {
-    res.send('Get all contacts')
-})
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 // @route      POST api/contacts
 // @desc      Add new contacts
 // @access     Private
-router.post('/',(req,res) => {
-    res.send('Add contacts')
-})
+router.post(
+  '/',
+  [auth, body('name', 'Name is required').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    const {name, email, phone, type} = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 // @route      PUT api/:id
 // @desc      update contact
 // @access     Private
-router.put('/:id',(req,res) => {
-    res.send('Update contact')
-})
+router.put('/:id', auth, async (req, res) => {
+  const {name, email, phone, type} = req.body;
+
+  // build a contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({msh: 'Contact not found'});
+
+    // make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not authorized'});
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      {$set: contactFields},
+      {new: true}
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 // @route      DELETE api/:id
 // @desc     Delete conact
 // @access     Private
-router.delete('/:id',(req,res) => {
-    res.send('Delete contact')
-})
+router.delete('/:id',auth, async (req, res) => {
 
+    try {
+        let contact = await Contact.findById(req.params.id);
+        if (!contact) return res.status(404).json({msh: 'Contact not found'});
+    
+        // make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+          return res.status(401).json({msg: 'Not authorized'});
+        }
+        await Contact.findOneAndRemove(req.params.id)
+    
+        res.json({msg: 'Contact removed'});
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
 
+// conect database
+connectDB()
+
+// Init middleware
+app.use(express.json({extended:false}))
 
 app.get('/',(req,res)=> res.json({msg: 'Welcome to the ContactKeeper API'}))
 
@@ -15,4 +21,4 @@ app.use('/api/contacts', require('./Routes/contacts'))
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`)
-)
+) 
